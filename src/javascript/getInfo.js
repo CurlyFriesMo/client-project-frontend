5c254e88928a5727487db09d1118d4cfa02c9387
CurlyFriesMo <jeroen.eillebrecht@gmail.com>
2022-06-22 15:19:04 +0200

    artist display fixed

--- src/javascript/getInfo.js
+++ src/javascript/getInfo.js
@@ -5,18 +5,35 @@ document.addEventListener("DOMContentLoaded", function(event) {
             if(data.length === 0){
                 console.log("empty list")
             }else{
-                console.log("list length: " + data.length)
-                document.getElementsByClassName("artistname1")[0].textContent = data[0].name
-                document.getElementById("genre1").textContent = data[1].genres[0]
+                let PTartistBlocks = document.getElementsByClassName('blokArtistPersonal')
+                let FTartistBlocks = document.getElementsByClassName('blokArtistFull')
 
-                document.getElementsByClassName("artistname2")[0].textContent = data[1].name
-                document.getElementById("genre2").textContent = data[1].genres[0]
+                let avg = 0
+                data.forEach(data => {
+                    avg += data.avgDistance
+                })
+                avg = (avg/data.length)
 
-                document.getElementsByClassName("artistname3")[0].textContent = data[2].name
-                document.getElementById("genre3").textContent = data[2].genres[0]
+                for(let i = 0; i < PTartistBlocks.length; i++){
+                    for (let j = 0; j < 7; j++) {
+                        if(data[j].name === PTartistBlocks[i].children[1].innerHTML){
+                            PTartistBlocks[i].style.display = 'block'
+                            if(data[j].genres[0] != undefined){
+                                PTartistBlocks[i].children[2].innerHTML = data[j].genres[0]
+                            }else{
+                                PTartistBlocks[i].children[2].innerHTML = ""
+                            }
+                            for (let i = 0; i < FTartistBlocks.length; i++) {
+                                if(FTartistBlocks[i].children[0].innerHTML === data[j].name){
+                                    FTartistBlocks[i].style.backgroundColor = "white"
+                                }
+                            }
 
-                document.getElementsByClassName("artistname4")[0].textContent = data[3].name
-                document.getElementById("genre4").textContent = data[3].genres[0]
+                        }
+                    }
+                }
+
+
             }
         }
     )
